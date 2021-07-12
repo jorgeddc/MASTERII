408ea39faa986f347deb1e7dc0c52c6a04965f5b
Jorge <jdiaz@masterii.com>
2021-07-12 13:20:22 +0200

    home y asociados ok

--- JavaScriptNavegador.js
+++ JavaScriptNavegador.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", ()=> {
 const container = document.querySelector(".informacion")
 const redesSociales = document.querySelector(".RRSS")
 const navegador=document.getElementById("navegador")
+const itemslidefondo1 = document.querySelector(".itemslidefondo1")
+const itemslidefondo2 = document.querySelector(".itemslidefondo2")
+const itemslidefondo3 = document.querySelector(".itemslidefondo3")
+
 let cont=0;
+
 gsap.from(".parrafo1", { opacity: 0, duration: 1, delay: 2.5, y: -10 })
 gsap.from(".parrafo2", { opacity: 0, duration: 1, delay: 2, y: -10 })
+
+
+
 const bar = document.querySelector(".bar")
 let buttons = document.querySelector('.navegador')
 let btn=buttons.querySelectorAll('.itemNavegador')
@@ -81,7 +89,7 @@ cont++
 navegador.addEventListener("click", ()=>{
 
      if (cont%2==0)
-     {  console.log("clik")
+     {  
     navegador.style.transform=" rotate(90deg)" 
      document.querySelector(".contenedor-responsive").style.height="200px"
      document.querySelector(".contenedor-responsive").style.top="30px"
@@ -95,15 +103,29 @@ navegador.addEventListener("click", ()=>{
     navegador.style.transform=" rotate(0deg)"
     document.querySelector(".contenedor-responsive").style.height="0px"
     document.querySelector(".contenedor-responsive").style.top="300px"
- 
-
-
- 
-
      }
      cont++
 })
-  
+
+  document.querySelector(".pulsa").addEventListener("click", ()=>{
+       console.log("click")
+     document.querySelector(".contenedor-responsive").style.height="0px"
+     document.querySelector(".contenedor-responsive").style.top="300px"
+     navegador.style.transform=" rotate(0deg)"
+     cont ++;
+
+  })
+
+  document.querySelector(".itemslide1").addEventListener("click",()=>{
+  itemslidefondo1.classList.toggle("vista")
+  })
+  document.querySelector(".itemslide2").addEventListener("click",()=>{
+     itemslidefondo2.classList.toggle("vista")
+     })
+     document.querySelector(".itemslide3").addEventListener("click",()=>{
+          itemslidefondo3.classList.toggle("vista")
+          })
+
 })
 
 
